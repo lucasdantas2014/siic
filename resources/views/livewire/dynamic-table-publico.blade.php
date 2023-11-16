d31650e880da36d7ec7ca5fe9e3f664622255f51
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2023-11-16 11:09:26 -0300

    ajuste tela publica

--- resources/views/livewire/dynamic-table-publico.blade.php
+++ resources/views/livewire/dynamic-table-publico.blade.php
@@ -1,12 +1,9 @@
 <div>
     {{-- Care about people's approval and you will be their prisoner. --}}
-    <div class="col d-flex flex-column align-items-center">
-
-        <h3 class = 'px-5 py-3'>Salas Reservadas</h3>
-
-        <div class="div-filtros row">
-
-            <div class="input-group col-4">
+    <div>
+        <h3 class ='px-5 py-3'>Salas Reservadas</h3>
+        <div id="div-filtros" class="row">
+            <div class="col-md-4">
                 <label for="categoria">Categoria: </label>
                 <select name="categoria" id ="categoria" wire:model="categoria" class = "form-control">
                     <option selected value = "">Todas as Categorias</option>
@@ -17,43 +14,48 @@
                 </select>
             </div>
 
-            <div class="input-group col-4">
+            <div class="col-md-4">
                 <label for="nome-sala">Sala: </label>
                 <input id="nome-sala" class="form-control" type="text" name="nome-sala" wire:model="nomeSala" placeholder="Buscar pelo nome da sala...">
             </div>
 
-            <div class="input-group col-4">
+            <div class="col-md-4">
                 <label for="nome-pessoa">Nome: </label>
                 <input id="nome-pessoa" class="form-control" type="text" name="nome-pessoa" wire:model="nomePessoa" placeholder="Buscar pelo nome da pessoa...">
             </div>
         </div>
 
-        <div class="container pt-4">
+        <div class="container">
             <table class="table">
-                    <thead>
+                <thead>
+                    <tr>
+                        <th scope="col">Sala</th>
+                        <th scope ="col">Nome</th>
+                        <th scope="col">Sala</th>
+                        <th scope="col">Hora da reserva</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($pedidos as $pedido)
                         <tr>
-                            <th scope="col">Sala</th>
-                            <th scope ="col">Nome</th>
-                            <th scope="col">Chave</th>
-                            <th scope="col">Hora da reserva</th>
+                            <th>{{$pedido->chave->sala->nome}}</td>
+                            <td>{{$pedido->user->nome}}</td>
+                            <td>{{$pedido->chave->sala->nome}}</td>
+                            <td>{{\Carbon\Carbon::parse($pedido->created_at)->format('d/m/Y H:i:s')}}</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        {{ $pedidos ?? 'a' }}
-                        @foreach($pedidos as $pedido)
-                            <tr>
-                                <th>{{$pedido->chave->sala->nome}}</td>
-                                <td>{{$pedido->user->nome}}</td>
-                                <td>{{$pedido->chave->nome}}</td>
-                                <td>{{\Carbon\Carbon::parse($pedido->created_at)->format('d/m/Y H:i:s')}}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                    @endforeach
+                </tbody>
+            </table>
 
-                <div>
-                    {{ $pedidos->links() }}
-                </div>
+            <div>
+                {{ $pedidos->links() }}
+            </div>
         </div>
     </div>
+
+    <style>
+        #div-filtros {
+            padding: 10px;
+        }
+    </style>
 </div>
--- resources/views/publico/index_publico.blade.php
+++ resources/views/publico/index_publico.blade.php
@@ -5,54 +5,112 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Chaves - SIIC</title>
-    <link rel="stylesheet" href="/css/styles.css">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    @vite(['resources/css/admin.css'])
+    @vite(['resources/js/app.js', 'resources/sass/app.scss'])
 
-    <link href="{{ asset('css/publico.css') }}" rel="stylesheet">
+    @yield('custom-head')
 
-    @livewireStyles()
+    @livewireStyles
 </head>
 <body>
-    <!-- CABEÇALHO -->
-    <div class="container border"  id = "header" style = "width:998px;background-image:url(background.png);">
-            <div class="row" style = "width:100%;">
-                <div class = "col-2">
-                    <img src="logo_campus.png" class = "p-3">
-                </div>
-                <div class = "col-3 offset-5">
-                    <h1 class = "mt-4">Sistema de chaves</h1>
-                </div>
-                <div id="div-login-link" class="col-2">
-                    <a  id="a-login" href="{{route('login_page')}}"> Login</a>
+
+    <div id="div-main" class="row">
+        <div class="container offset-2 col-md-8">
+            <div id="div-header">
+                <div class="row">
+                    <div class="col">
+                        <img src="logo_campus.png" class = "p-3">
+                    </div>
+                    <div id="div-titulo" class= "col">
+                        <h1 class = "mt-4">SIIC - Sistema Interno Integrado de Chaves</h1>
+                    </div>
+                    <div id="div-login-link" class="col-2">
+                        <a  id="a-login" href="{{route('login_page')}}"> Login</a>
+                    </div>
                 </div>
             </div>
-        <!-- LISTA DE OPÇÕES -->
-
-    </div>
-    <!-- CORPO -->
-    <div class="container" style = "width:998px;">
-        <div class="row justify-content-center align-items-center ">
-            <!-- LISTA DE PEDIDOS -->
-            <livewire:dynamic-table-publico />
-
+            <div id="div-conteudo" class="row">
+                <livewire:dynamic-table-publico />
+            </div>
         </div>
-    </div>
-    @livewireScripts()
 
+        <div id="div-footer" class="col-md-12">
+            @include('layouts.footer_verde')
+        </div>
+
+    </div>
+
+    @livewireScripts
     <style>
+
+        body {
+            background-color: #F6F5F4;
+            height: 100vh;
+            overflow: hidden;
+            font-size: 13px;
+        }
+
+        #div-main {
+            height: 100%;
+        }
+
+        #header {
+            background-image: url(background.png);
+        }
+
+        #div-header {
+            background-image:url(background.png);
+        }
+
+        .container {
+            background-color: #fff;
+            height: 100%;
+            padding: 0;
+        }
+
+        #div-titulo {
+            align-items: center;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: center;
+        }
 
         #div-login-link {
             display: flex;
             text-align: right;
             font-weight: bolder;
         }
+
         #a-login {
             text-decoration: none;
             color: #5f5757f0;
             font-size: 20px
         }
+
+        .pagination > li > a,
+    .pagination > li > span {
+        color: #3EA14E; // use your own color here
+    }
+
+    .pagination > .active > a,
+    .pagination > .active > a:focus,
+    .pagination > .active > a:hover,
+    .pagination > .active > span,
+    .pagination > .active > span:focus,
+    .pagination > .active > span:hover {
+        background-color: #3EA14E;
+        border-color: #3EA14E;
+    }
+
+    .page-item button{
+        color: #3EA14E !important;
+    }
+
+    nav {
+        height: fit-content;
+    }
 
     </style>
 </body>
